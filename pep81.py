ae6799ecc6e46ec21f9eb180259439acea6ecb32
sambapython@gmail.com <sambapython@gmail.com>
2017-01-10 09:23:04 +0530

    first commit

---
+++ pep81.py
@@ -0,0 +1,13 @@
+a = [10, 20, 30, 40]
+
+
+def fun():
+    print "this is funciton"
+
+
+def fun1():
+    print "this is fun1"
+print "ssssssssssssssssssssssssssssssssssssssssssssss\
+sssssssssssssssssssssssssssssssssssssssssssssssssssssssssss"
+fun()
+fun1()
